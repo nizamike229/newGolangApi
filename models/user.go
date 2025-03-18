5091b6917b05d8ba19e45f94c7aafa154bb160f8
00061377 <00061377@DABP-00061377>
2025-03-18 14:09:16 +0500

    Created Models for User and functions to Hash and Check Passwords

---
+++ models/user.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+	"awesomeProject/logger"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username string `gorm:"unique;not null"`
+	Password string `gorm:"not null"`
+}
+
+type UserRequest struct {
+	Username string `json:"username" validate:"required,min=3,max=32"`
+	Password string `json:"password" validate:"required,min=8,max=64"`
+}
+
+func (u *User) HashPassword(password string) error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		logger.Error(err.Error())
+		return err
+	}
+	u.Password = string(bytes)
+	return nil
+}
+
+func (u *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}
