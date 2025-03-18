7a01f53508f3b9f72913261772028e75d1ce98d1
00061377 <00061377@DABP-00061377>
2025-03-18 14:12:10 +0500

    Add custom auth middleware to secure some endpoint

---
+++ customMiddleware/auth.go
@@ -0,0 +1,41 @@
+package customMiddleware
+
+import (
+	"context"
+	"github.com/golang-jwt/jwt/v5"
+	"net/http"
+)
+
+var jwtSecret = []byte("my-super-secret-string")
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("authToken")
+		if err != nil {
+			http.Error(w, "Missing token in cookie", http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := cookie.Value
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, http.ErrAbortHandler
+			}
+			return jwtSecret, nil
+		})
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			http.Error(w, "Invalid token claims", http.StatusUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "claims", claims)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
