f9117f66f3a294471fbc7c90dd10db364fcf0744
00061377 <00061377@DABP-00061377>
2025-03-19 09:35:14 +0500

    Removed redundant field

--- models/taskRequest.go
+++ models/taskRequest.go
@@ -3,8 +3,7 @@ package models
 import "time"
 
 type TaskRequest struct {
-	Title     string    `json:"title" validate:"required,min=1,max=100"`
-	Priority  Priority  `json:"priority" validate:"gte=0,lte=2"`
-	Deadline  time.Time `json:"deadline" validate:"required"`
-	Completed bool      `json:"completed"`
+	Title    string    `json:"title" validate:"required,min=1,max=100"`
+	Priority Priority  `json:"priority" validate:"gte=0,lte=2"`
+	Deadline time.Time `json:"deadline" validate:"required"`
 }
